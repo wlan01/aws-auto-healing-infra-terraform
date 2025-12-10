2570a4163ba5924950bfdb25a7e34423a3eb8d43
Gokul <79099097+wlan01@users.noreply.github.com>
2025-12-11 04:50:08 +0530

    Create module/vpc/variables.tf

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,14 @@
+variable "project_name" {
+  description = "Project name prefix for resource tags"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
+}
