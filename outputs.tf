3d39cf8942d294676d19cee205c0265e4145ffd7
Gokul <79099097+wlan01@users.noreply.github.com>
2025-12-11 04:48:58 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,9 @@
+output "vpc_id" {
+  description = "ID of the created VPC"
+  value       = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  description = "List of public subnet IDs"
+  value       = module.vpc.public_subnet_ids
+}
