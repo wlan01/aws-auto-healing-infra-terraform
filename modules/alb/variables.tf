0ab456ac2b6abdd5b248cb4586f85cf20161bed1
Gokul <79099097+wlan01@users.noreply.github.com>
2025-12-12 05:27:41 +0530

    Create variables.tf

---
+++ modules/alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "public_subnet_ids" {
+  type = list(string)
+}
+
+variable "alb_sg_id" {
+  type = string
+}
