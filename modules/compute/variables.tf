175a66b7ce124fd292fbb618a3db28cf2d67a953
Gokul <79099097+wlan01@users.noreply.github.com>
2025-12-12 05:30:31 +0530

    Create variables.tf

---
+++ modules/compute/variables.tf
@@ -0,0 +1,35 @@
+variable "project_name" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "subnet_ids" {
+  type = list(string)
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "desired_capacity" {
+  type = number
+}
+
+variable "min_size" {
+  type = number
+}
+
+variable "max_size" {
+  type = number
+}
+
+variable "ec2_sg_id" {
+  type = string
+}
+
+variable "target_group_arn" {
+  type = string
+}
